4b6f37e8ca0d673e397c43ac816e684311d57c52
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-03-24 16:53:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
     //Write your code here
+	let table = document.querySelector("#myTable");
+	const rows =parseInt(prompt("Enter the no of rows"));
+	const columns =parseInt(prompt("Enter the no of columns"));
+	// alert(typeof rows)
+		if(typeof rows=="number" && typeof columns=="number" && rows>0 && columns>0){
+	for(let i=0;i<rows;i++){
+		let rowsInsert = document.createElement("tr");
+		  for(let j=0;j<columns;j++){
+          let columnsInsert = document.createElement("td");
+			  columnsInsert.innerText =`row${i+1} columns ${j+1}`;
+			  rowsInsert.appendChild(columnsInsert);
+		  }
+		table.appendChild(rowsInsert)
+	}
+		}
   
 }
