8a0950b72d91e0163a9785e7974a838212f1973f
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-03-24 17:03:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 function createTable() {
     //Write your code here
 	let table = document.querySelector("#myTable");
-	const rows =parseInt(prompt("Enter the no of rows"));
-	const columns =parseInt(prompt("Enter the no of columns"));
+	const rows =parseInt(prompt("Input number of rows"));
+	const columns =parseInt(prompt("Input number of columns"));
 	// alert(typeof rows)
-		if(typeof rows=="number" && typeof columns=="number" && rows>0 && columns>0){
+		if(isNaN(rows) || isNaN(columns) || rows<=0 || columns<=0){
+			alert("Please enter valid positive numbers!");
+			return 
+		}
 	for(let i=0;i<rows;i++){
 		let rowsInsert = document.createElement("tr");
 		  for(let j=0;j<columns;j++){
           let columnsInsert = document.createElement("td");
-			  columnsInsert.innerText =`row${i} columns ${j}`;
+			  columnsInsert.innerText =`Row${i} Columns ${j}`;
 			  rowsInsert.appendChild(columnsInsert);
 		  }
 		table.appendChild(rowsInsert)
 	}
-		}
+		
   
 }
